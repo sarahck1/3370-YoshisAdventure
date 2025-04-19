b41894c2ef15ee9b2f7371ee260eba7404706b79
Evelyn Lechuga <evelynlechuga07@gmail.com>
2025-04-19 00:53:12 -0500

    Fixed camera and added finish line

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLine : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    /*void Start()
+    {
+        
+    }*/
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }*/
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Allows level progression if neccessary
+        }
+    }
+}
--- Assets/Scripts/follow_yoshi.cs
+++ Assets/Scripts/follow_yoshi.cs
@@ -6,16 +6,35 @@ public class follow_yoshi : MonoBehaviour
 {
     public float FollowSpeed = 2f;
     public Transform target;
-    public float fixedY = 0f;
+    public float zOffset = -10f;
+
+    public Vector2 deadZone = new Vector2(1f, 1f);
 
     // Update is called once per frame
     void Update()
     {
         if (target != null)
         {
-        float newX = Mathf.Lerp(transform.position.x, target.position.x, FollowSpeed * Time.deltaTime);
-        transform.position = new Vector3(newX, fixedY, -10f);
-        }
+            Vector3 currentPos = transform.position;
+            Vector3 targetPos = target.position;
 
+            float xDiff = Mathf.Abs(currentPos.x - targetPos.x);
+            float yDiff = Mathf.Abs(currentPos.y - targetPos.y);
+
+            float newX = currentPos.x;
+            float newY = currentPos.y;
+
+            if (xDiff > deadZone.x)
+            {
+                newX = Mathf.Lerp(currentPos.x, targetPos.x, FollowSpeed * Time.deltaTime);
+            }
+
+            if (yDiff > deadZone.y)
+            {
+                newY = Mathf.Lerp(currentPos.y, targetPos.y, FollowSpeed * Time.deltaTime);
+            }
+
+            transform.position = new Vector3(newX, newY, zOffset);
+        }
     }
 }
